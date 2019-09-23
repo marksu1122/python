d9c72aac188732c08fb6bc03d0075bad166f152c
Mark <mark6107@hotmail.com>
2019-09-23 14:04:35 +0800

    >

---
+++ hw1.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+#显示所有列
+pd.set_option('display.max_columns', None)
+#显示所有行
+pd.set_option('display.max_rows', None)
+#设置value的显示长度为100，默认为50
+pd.set_option('max_colwidth',15)
+
+pd_data = pd.read_csv('/Users/marksu/Downloads/Reviews.csv')[:10000]
+
+df = pd.DataFrame(pd_data)
+
+#print(df.duplicated(['UserId']))
+#print(df[df.duplicated(['UserId'])].count())
+
+#pd_data.sort_values(by=['UserId'])
+
+#df = df.drop('HelpfulnessNumerator', 1)
+df = df.drop(df.columns[[4,5,7,8,9]],1) 
+count = df.groupby('UserId').count()
+count = count.drop(count.columns[[1,2,3]], axis=1) 
+count.rename(columns={'Id':'Count'},inplace = True)
+df = pd.merge(df, count, on=['UserId'])
+df.sort_values(by = ['Count','UserId'],inplace = True,ascending=False)
+mean = df.groupby('UserId')['Score'].agg(['mean', 'sum'])
+df = pd.merge(df, mean, on=['UserId'])
+df=df.drop_duplicates(['UserId'])
+
+# print(df)
+print (df)
+#df.insert(4,column='times',value=df.groupby(['UserId']).count())
+
+#print(pd_data.sort_values(by=['UserId']))
+#print(pd_data)
+
+    
+
+
+"""
+import csv
+with open('/Users/marksu/Downloads/Reviews.csv') as csvfile:
+    rows = csv.reader(csvfile)
+    date=[]    
+    i = 0
+    for item in rows:
+        date.append(item)    
+  
+    while i < 10:
+        print (date[i])    
+        i = i+1
+
+
+
+#显示所有列
+pd.set_option('display.max_columns', None)
+#显示所有行
+pd.set_option('display.max_rows', None)
+#设置value的显示长度为100，默认为50
+pd.set_option('max_colwidth',100)
+
+"""
--- pratice1.py
+++ pratice1.py
@@ -1,1 +1,30 @@
-print("hello")
+import pandas as pd
+'''
+ipl_data = {'Team': ['Riders', 'Riders', 'Devils', 'Devils', 'Kings',
+         'kings', 'Kings', 'Kings', 'Riders', 'Royals', 'Royals', 'Riders'],
+         'Rank': [1, 2, 2, 3, 3,4 ,1 ,1,2 , 4,1,2],
+         'Year': [2014,2015,2014,2015,2014,2015,2016,2017,2016,2014,2015,2017],
+         'Points':[876,789,863,673,741,812,756,788,694,701,804,690]}
+df = pd.DataFrame(ipl_data)
+
+df.sort_values(by=['Team'])
+df.groupby(['cluster']).mean()
+print (df)
+'''
+df = pd.DataFrame({'A': ['a', 'b', 'a', 'c', 'a', 'c', 'b', 'c'], 
+                       'B': [2, 8, 1, 4, 3, 2, 5, 9], 
+                       'C': [102, 98, 107, 104, 115, 87, 92, 123]})
+print (df)
+print ("==================================")
+count = df.groupby('A').count()
+
+
+#count = count.drop(count.columns[[1]], axis=1) 
+print (count)
+#df.insert(4,column='times',value=count['B'])
+
+
+print (pd.merge(df, count, on=['A']))
+
+
+#print (df.join(count, on='B'))
