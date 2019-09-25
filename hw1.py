c83d349173e91bcad96c5fe558d7144dc3a18c0c
Mark <mark6107@hotmail.com>
2019-09-25 15:18:21 +0800

    commit

--- hw1.py
+++ hw1.py
@@ -1,43 +1,20 @@
 import pandas as pd
-import numpy as np
 from pandas import DataFrame
 
 pd_data = pd.read_csv('/Users/marksu/Downloads/Reviews.csv', index_col=0)[:10000]
 
 df = pd.DataFrame(pd_data)
-df = df.drop(df.columns[[0,3,4,6,7,8]],1) 
+df = df.drop(df.columns[[0,3,4,6,7,8]],axis=1) 
+part_one =df.groupby('UserId')['Score'].agg({'count','mean'})
 
-count = df.groupby('UserId').count()
-count = count.drop(['Score'],1) 
-count.rename(columns={'ProfileName':'Count'},inplace = True)
-df = pd.merge(df, count, on=['UserId'])
+df = pd.merge(df, part_one, on=['UserId'])
+df.sort_values(by = ['count','UserId'],inplace = True,ascending=False)
 
-df.sort_values(by = ['Count','UserId','ProfileName'],inplace = True,ascending=False)
-mean = df.groupby('UserId')['Score'].mean()
-df = pd.merge(df, mean, on=['UserId'])
-
-df=df.drop_duplicates(['UserId']).drop(['Score_x'],1).head(10)
-df.rename(columns={'Score_y':'Score mean'},inplace = True)
+df=df.drop_duplicates(['UserId']).drop(['Score'],1).head(10)
 df = df.reset_index(drop=True)
-print (df)
+print(df)
+
 #df.insert(4,column='times',value=df.groupby(['UserId']).count())
 #print(df.duplicated(['UserId']))
 #print(df[df.duplicated(['UserId'])].count())
 
-    
-
-
-"""
-import csv
-with open('/Users/marksu/Downloads/Reviews.csv') as csvfile:
-    rows = csv.reader(csvfile)
-    date=[]    
-    i = 0
-    for item in rows:
-        date.append(item)    
-  
-    while i < 10:
-        print (date[i])    
-        i = i+1
-
-"""
