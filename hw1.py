62640a3fb36696357fd44ad864b66b4f86e66952
Mark <mark6107@hotmail.com>
2019-09-27 22:16:21 +0800

    commir

--- hw1.py
+++ hw1.py
@@ -1,16 +1,18 @@
 import pandas as pd
 from pandas import DataFrame
-
+#讀取csv檔
 pd_data = pd.read_csv('/Users/marksu/Downloads/Reviews.csv', index_col=0)[:10000]
 
 df = pd.DataFrame(pd_data)
+#刪除不用的欄位,根據'UserId'進行分群後對'Score'進行mean &count
 df = df.drop(df.columns[[0,3,4,6,7,8]],axis=1) 
 part_one =df.groupby('UserId')['Score'].agg({'count','mean'})
-
+#merge two dataframe
 df = pd.merge(df, part_one, on=['UserId'])
+#排序後選出前十名
 df.sort_values(by = ['count','UserId'],inplace = True,ascending=False)
-
 df=df.drop_duplicates(['UserId']).drop(['Score'],1).head(10)
+
 df = df.reset_index(drop=True)
 print(df)
 
--- pratice1.py
+++ pratice1.py
@@ -1,30 +1,8 @@
 import pandas as pd
-'''
-ipl_data = {'Team': ['Riders', 'Riders', 'Devils', 'Devils', 'Kings',
-         'kings', 'Kings', 'Kings', 'Riders', 'Royals', 'Royals', 'Riders'],
-         'Rank': [1, 2, 2, 3, 3,4 ,1 ,1,2 , 4,1,2],
-         'Year': [2014,2015,2014,2015,2014,2015,2016,2017,2016,2014,2015,2017],
-         'Points':[876,789,863,673,741,812,756,788,694,701,804,690]}
-df = pd.DataFrame(ipl_data)
+import numpy as np
 
-df.sort_values(by=['Team'])
-df.groupby(['cluster']).mean()
-print (df)
-'''
-df = pd.DataFrame({'A': ['a', 'b', 'a', 'c', 'a', 'c', 'b', 'c'], 
-                       'B': [2, 8, 1, 4, 3, 2, 5, 9], 
-                       'C': [102, 98, 107, 104, 115, 87, 92, 123]})
-print (df)
-print ("==================================")
-count = df.groupby('A').count()
-
-
-#count = count.drop(count.columns[[1]], axis=1) 
-print (count)
-#df.insert(4,column='times',value=count['B'])
-
-
-print (pd.merge(df, count, on=['A']))
-
-
-#print (df.join(count, on='B'))
+# numpy.array(object, dtype = None, copy = True, order = 'K', subok = False, ndmin = 0)
+dt1 = [1, 2, 3, 4, 5, '6']
+arr1 = np.array(dt1,subok= False)
+print (arr1)
+print (arr1[0].dtype())
