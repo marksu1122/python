b5c43b49d5dcbb10ebdde5ec00a1e3f9d7428721
Mark <mark6107@hotmail.com>
2019-10-03 23:16:31 +0800

    half

--- dataminingHw1.py
+++ dataminingHw1.py
@@ -1,46 +1,43 @@
 import itertools
 
-
 #itemsets = [['A','C','D'],['B','C','E'],['A','B','C','E'],['B','E']]
 itemsets = [['Bread','Milk'],['Bread','Diaper','Beer','Eggs'],['Milk','Diaper','Beer','Coke'],['Bread','Milk','Diaper','Beer'],['Bread','Milk','Diaper','Coke']]
-dic_test={}
-minimumSup = 3
-#C1
-for itemset in itemsets:
-    for item in itemset:
-        count = dic_test.get(item)
-        if(count != None):
-            count += 1
-            dic_test.update({item:count})
-        else: 
-            dict2 = {item: 1}
-            dic_test.update(dict2)                
-            
-#minimal support
-for key in dic_test.copy():
-    if(dic_test[key] < minimumSup):
-        del dic_test[key]
 
-#dic_test.update({('A','B'):0})
-#dic_test['John', 'Smith'] = 10000.0
-print(dic_test)
-
-#C2
-list1 = [] 
-list2 = [] 
-for key1 in dic_test:
-    list1.append(str(key1))
-list1.sort()
-for i in range(len(list1)):
-    for j in range(i+1,len(list1)):
-        list2.append([str(list1[i]),str(list1[j])])
-
-# search CK support
-def search(ck,dk):
+def C1(itemsets):
+    C1 = {}
     for itemset in itemsets:
-        for data in ck:
-            count = dk.get(tuple(data))
-            # TODO
+        for item in itemset:
+            item =tuple([item])
+            count = C1.get(item)            
+            if(count != None):
+                count += 1
+                C1.update({item:count})
+            else: 
+                C1.update({item: 1})
+    return C1
+
+# C1 to sortlist
+def C2(l1,itemsets):
+    listC1 =[]
+    listC2 =[]
+    C2 ={}
+    for key in l1:
+        #convert single tuple to string [print(str(key)) == ('Bread',) ]      
+        listC1.append("".join(key))
+        listC1.sort()
+    for s in itertools.combinations(listC1, 2):
+        listC2.append(list(s))
+    C2 = Search(listC2,itemsets)
+    return C2
+
+# search Ck support
+def Search(listCk,itemsets):
+    #TODO 前一步就產生CK??
+    ck={}
+    for itemset in itemsets:
+        for data in listCk:
+            count = ck.get(tuple(data))
+            # TODO count值處理
             if(count == None):
                 count = 0
             match = 0
@@ -51,25 +48,15 @@ def search(ck,dk):
                         break
                 if(match == len(data)):
                     count +=1
-            dk.update({tuple(data):count})
-    return dk        
-
-# produce LK
-def LK(dk):
-    for key in dk.copy():
-        if(dk[key] < minimumSup):
-            del dk[key]
-    return dk
+            ck.update({tuple(data):count})
+    return ck  
 
 
-
-
-
-# join parameters:list
-def join(dk):
+# JoinStep TODO 怪怪的
+def JoinStep(lk):
     listk = []
     newlist = []
-    for key in dk:
+    for key in lk:
         listk.append(list(key))
     listk.sort()
 
@@ -89,14 +76,14 @@ def join(dk):
                 newlist.append(temp)
     return newlist
 
-# TODO prune step
-def prune(listk,dk):
+# PruneStep
+def PruneStep(listk,lk):
     for data in listk:
         check = 0
         #subsetcheck
         for s in itertools.combinations(data, len(data)-1):
             notfound = True
-            for item in dk:
+            for item in lk:
                 # TODO 比較(list compare?)tuple
                 if(s == item):
                     check +=1
@@ -109,18 +96,41 @@ def prune(listk,dk):
             listk.remove(data)
     return listk
 
-# TODO forloop generate CK LK
-dic_1={}
-dic_2={}
-print(list2)
-dic_1 = search(list2,dic_1)
-print("==============>",dic_1)
-dic_1 = LK(dic_1)
-print("==============>",dic_1)
-listtest = join(dic_1)
-print("==============>",listtest)
-listtest = prune(listtest,dic_1)
-print("==============>",listtest)
-dic_2 = search(listtest,dic_2)
-print("==============>",dic_2)
+# produce LK check minmumSupport  //return dictionary
+def Lk(ck,minimumSup):
+    for key in ck.copy():
+        if(ck[key] < minimumSup):
+            del ck[key]
+    return ck
 
+
+
+def Apriori(itemsets):
+    #TODO itemset minmunsup conf
+    minimumSup = 2
+    conf = 0.5
+    c1 = C1(itemsets)
+    l1 = Lk(c1,minimumSup)
+    c2 = C2(l1,itemsets)
+    #TODO range
+    for k in range(2,1000):
+        #LK
+        locals()['l%s'%k] = Lk(locals()['c%s'%k],minimumSup)
+        #produce listL(k+1) JoinStep 
+        locals()['listL%s'%(k+1)] = JoinStep(locals()['l%s'%k])
+        #PruneStep listL(k+1) 
+        locals()['listL%s'%(k+1)] = PruneStep(locals()['listL%s'%(k+1)],locals()['l%s'%k])
+        #ck
+        locals()['c%s'%(k+1)] = Search(locals()['listL%s'%(k+1)],itemsets)
+
+        #TODO
+        if(not locals()['c%s'%(k+1)]):
+            for i in range(k,0,-1):
+                for key,value in locals()['l%s'%i].items():
+                    for s in itertools.combinations(key, len(key)):
+                        print("S=====>",s)
+
+                print('===L',i,'========>',locals()['l%s'%i])
+            break
+
+Apriori(itemsets)
